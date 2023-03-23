0034805c3648ce7442f7196de1f2db43084974bf
Ini <Ini>
2023-03-23 12:50:17 -0500

    matching registration

--- main.py
+++ main.py
@@ -3,37 +3,42 @@ import secrets
 import requests
 from forms import Search_Form, LoginForm, RegisterForm
 from flask import session, request
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template
 from flask import url_for, flash, redirect
 from flask_behind_proxy import FlaskBehindProxy
 from flask_login import LoginManager, UserMixin, login_user
 from flask_login import login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+import MySQLdb
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+
 
 # Create a flask app for the website
 app = Flask(__name__) 
-proxied = FlaskBehindProxy(app)
 
-# Assign the flask app an secret key
+#Add Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db' 
+# Secret key
 app.config['SECRET_KEY'] = secrets.token_hex(16)
+# Initialize The Database
+db = SQLAlchemy(app)
 
-#Database configuration and creating sqlalchemy object
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/auth'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy()
-
-#Create User Model
-
-class User(db.Model):
-
-    __tablename__ = 'usertable'
-
+# Create Model
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique = True)
-    email = db.Column(db.String(50), unique = True)
-    password = db.Column(db.String(256), unique=True)
+    username = db.Column(db.String(25), nullable=False)
+    email = db.Column(db.String(), nullable=False, unique=True)
+    password = db.Column(db.String(), nullable=False)
+
+    # Create A String
+    def __repr__(self):
+        return "<username %r>" % self.username
+    
+with app.app_context():
+    db.create_all()
+
+ 
 
 # The intro page
 @app.route("/intro", methods=['GET', 'POST'])
@@ -45,47 +50,40 @@ def intro():
 def home(user):
     print("home page")
 
+@app.route("/")
 # login page function
 @app.route('/login', methods=['GET', 'POST'])
+#Umair code goes here
 def login():
-    form = LoginForm(request.form)
-
+    form = LoginForm()
     return render_template('login.html', form=form, display="none", signup=url_for("signup"))
 
-@app.route("/")
 # signup page function
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    form = RegisterForm(request.form)
+    form = RegisterForm()
+    return render_template('signup.html', form=form, display="none", login=url_for("login"))
 
-    # Checking that method is post and form is valid or not.
-    if request.method == 'POST' and form.validate():
+@app.route('/register_success', methods=['GET', 'POST'])
+def success():
+    form = RegisterForm()
+    if request.method=="POST":
+        email_exists = db.session.query(db.session.query(Users).filter_by(email=form.email.data).exists()).scalar()
+        print(email_exists)
+        if email_exists == False:
+            user = Users(username=form.username.data, email=form.email.data, password=form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            form.username.data = ''
+            form.email.data = ''
+            form.password.data = ''
+            flash("User Added!")
+            return render_template('login.html', form=form, display="none", signup=url_for("signup"))
 
-        #storing a users password in plain text is just... stupid.... encript it :)
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-
-        # create new user model object
-        new_user = User(
-
-            username = form.username.data,
-            email = form.email.data,
-            password = hashed_password
-        )
-
-        # saving user object into data base with hashed password
-        db.session.add(new_user)
-
-        db.session.commit()
-
-        flash('Welcome to the cult', 'success')
-        
-        # if registration successful, then redirecting to login page
-        return redirect(url_for('login'))
-    
-    else:
-        # if method is Get, than render registration form
-        return render_template('signup.html', form=form, display="none", login=url_for("login"))
-
+        else:
+            flash("An account with this email already exists")
+            form.email.data = ''
+            return render_template('signup.html', form=form)
 # grocery index page function
 @app.route('/gindex', methods=['GET', 'POST'])
 def gindex():
@@ -109,5 +107,4 @@ def logout():
     return redirect(url_for('intro'))
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True, host="0.0.0.0")
